474569208dd4a6ab7d8d88db9e4642bf8e2e9705
Xavier Gonzalez <xavier@gonzalez.pm>
2015-10-03 14:17:01 +0200

    mise à jour des mail client

--- inc/inc.config.php
+++ inc/inc.config.php
@@ -7,11 +7,11 @@ define("DBPASSWD", "accleaner33");
 */
 // CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES CONSTANTES
 //mail parametrage
-$mailCustomer = "fjavi.gonzalez@gmail.com";
+$mailCustomer = "contact@votreimmopro.com";
 $mailNameCustomer = "VotreImmoPro";
-$facebookLink = "https://www.facebook.com";
+$facebookLink = "https://www.linkedin.com";
 
 //mail d'envoi
+$mailContact = "contact@votreimmopro.com";
 //$mailContact = "fjavi.gonzalez@gmail.com";
-$mailContact = "fjavi.gonzalez@gmail.com";
 $mailBcc = "fjavi.gonzalez@gmail.com,xav335@hotmail.com";
